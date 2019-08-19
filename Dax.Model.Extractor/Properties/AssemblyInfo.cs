daf1187795967ca23c80b282fe72f41e11f04f8c
marcorusso <marco.russo@loader.it>
2019-08-19 11:01:02 +0200

    Published NuGet Packages

--- Dax.Model.Extractor/Properties/AssemblyInfo.cs
+++ Dax.Model.Extractor/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dax.Model.Extractor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Populate a Dax.Model extracting data from TOM and Analysis Services.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SQLBI")]
 [assembly: AssemblyProduct("Dax.Model.Extractor")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
