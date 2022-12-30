f95228efadd43744a8dabd657682fb225791d5fc
Alberto Spelta <albertospelta@users.noreply.github.com>
2022-12-30 18:34:10 +0100

    Add more tests

---
+++ src/Dax.Model.Extractor.Tests/ModelFixture.cs
@@ -0,0 +1,29 @@
+﻿namespace Dax.Model.Extractor.Tests
+{
+    using Microsoft.AnalysisServices;
+    using System.IO;
+    using TOM = Microsoft.AnalysisServices.Tabular;
+
+    public class ModelFixture
+    {
+        private const string ContosoFilePath = @".\_data\bim\Contoso.bim";
+        private const string TestModel1FilePath = @".\_data\bim\TestModel1.bim";
+
+        public ModelFixture()
+        {
+            Contoso = GetModel(ContosoFilePath);
+            TestModel1 = GetModel(TestModel1FilePath, CompatibilityMode.PowerBI);
+        }
+
+        private TOM.Model GetModel(string path, CompatibilityMode mode = CompatibilityMode.Unknown)
+        {
+            var json = File.ReadAllText(path);
+            var database = TOM.JsonSerializer.DeserializeDatabase(json, mode: mode);
+            return database.Model;
+        }
+
+        public TOM.Model Contoso { get; private set; }
+
+        public TOM.Model TestModel1 { get; private set; }
+    }
+}
--- src/Dax.Model.Extractor.Tests/TomExtractorTests.cs
+++ src/Dax.Model.Extractor.Tests/TomExtractorTests.cs
@@ -3,36 +3,88 @@ namespace Dax.Model.Extractor.Tests
     using Dax.Metadata.Extractor;
     using Microsoft.AnalysisServices;
     using System;
-    using System.IO;
+    using System.Linq;
     using Xunit;
-    using TOM = Microsoft.AnalysisServices.Tabular;
 
-    public class TomExtractorTests
+    public class TomExtractorTests : IClassFixture<ModelFixture>
     {
-        private const string ContosoBimPath = @".\_data\bim\Contoso.bim";
+        private readonly ModelFixture _fixture;
+
+        public TomExtractorTests(ModelFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
-        public void GetDaxModel_PopulateTest()
+        public void GetDaxModel_Populate_ContosoTest()
         {
-            var tomModel = GetTestTomModel();
-            var daxModel = TomExtractor.GetDaxModel(tomModel, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
+            var daxModel = TomExtractor.GetDaxModel(_fixture.Contoso, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
 
             Assert.Equal(10, daxModel.Tables.Count);
             Assert.Equal(10, daxModel.Relationships.Count);
             Assert.Single(daxModel.Roles);
 
             Assert.Equal(1550, daxModel.CompatibilityLevel);
-            Assert.NotNull(daxModel.CompatibilityMode);
+            Assert.Equal(CompatibilityMode.Unknown.ToString(), daxModel.CompatibilityMode);
             Assert.Equal(DateTime.MinValue, daxModel.LastProcessed);
             Assert.Equal(DateTime.MinValue, daxModel.LastUpdate);
             Assert.Equal(0, daxModel.Version);
             Assert.True((DateTime.UtcNow - daxModel.ExtractionDate) < TimeSpan.FromSeconds(10));
         }
 
-        private TOM.Model GetTestTomModel()
+        [Fact]
+        public void GetDaxModel_Populate_TestModel1Test()
         {
-            var database = TOM.JsonSerializer.DeserializeDatabase(File.ReadAllText(ContosoBimPath));
-            return database.Model;
+            var daxModel = TomExtractor.GetDaxModel(_fixture.TestModel1, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
+
+            Assert.Single(daxModel.Tables);
+            Assert.Empty(daxModel.Relationships);
+            Assert.Empty(daxModel.Roles);
+
+            Assert.Equal(1550, daxModel.CompatibilityLevel);
+            Assert.Equal(CompatibilityMode.Unknown.ToString(), daxModel.CompatibilityMode);
+        }
+
+        [Fact]
+        public void GetDaxModel_ColumnProperty_IsHidden_Test()
+        {
+            var daxModel = TomExtractor.GetDaxModel(_fixture.TestModel1, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
+
+            var table = daxModel.Tables.SingleOrDefault((t) => t.TableName.Name == "CalculatedTable1");
+            Assert.NotNull(table);
+
+            var column = table.Columns.SingleOrDefault((c) => c.ColumnName.Name == "Name-IsHidden");
+            Assert.NotNull(column);
+            Assert.True(column.IsHidden);
+        }
+
+        [Fact]
+        public void GetDaxModel_ColumnProperty_SortByColumn_Test()
+        {
+            var daxModel = TomExtractor.GetDaxModel(_fixture.TestModel1, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
+
+            var table = daxModel.Tables.SingleOrDefault((t) => t.TableName.Name == "CalculatedTable1");
+            Assert.NotNull(table);
+
+            var column = table.Columns.SingleOrDefault((c) => c.ColumnName.Name == "Name-SortByColumn");
+            Assert.NotNull(column);
+            Assert.Equal("Index", column.SortByColumnName.Name);
+        }
+
+        [Fact]
+        public void GetDaxModel_ColumnProperty_GroupByColumns_Test()
+        {
+            var daxModel = TomExtractor.GetDaxModel(_fixture.TestModel1, extractorApp: "VPAXUnitTest", extractorVersion: "1.0.0.0");
+
+            var table = daxModel.Tables.SingleOrDefault((t) => t.TableName.Name == "CalculatedTable1");
+            Assert.NotNull(table);
+
+            var column = table.Columns.SingleOrDefault((c) => c.ColumnName.Name == "Name-GroupByColumns");
+            Assert.NotNull(column);
+            Assert.Single(column.GroupByColumns);
+
+            var groupingColumn = column.GroupByColumns[0];
+            Assert.Equal("Index", groupingColumn.Name);
         }
     }
 }
