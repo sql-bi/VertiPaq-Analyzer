620e79d7a180abc7ea52c3f036929d0d56877475
Sergio Murru <sergio.murru@loader.it>
2023-04-10 17:07:37 +0200

    added ConsumersCollection

---
+++ src/Dax.Consumer/ConsumersCollection.cs
@@ -0,0 +1,15 @@
+﻿using Dax.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dax.Consumer
+{
+    public class ConsumersCollection : List<Consumer>
+    {
+        public DaxName Path { get; set; }
+    }
+}
