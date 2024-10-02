483ac225bdbe2420734a74ddf328a0366bf3f1b5
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-10-02 18:30:52 +0200

    Refactor to add DebuggerDisplay attributes to Dax.Metadata classes

--- src/Dax.Metadata/UserHierarchy.cs
+++ src/Dax.Metadata/UserHierarchy.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Dax.Metadata
 {
+    [DebuggerDisplay("{HierarchyName.Name,nq}")]
     public class UserHierarchy
     {
         public Table Table { get; set; }
