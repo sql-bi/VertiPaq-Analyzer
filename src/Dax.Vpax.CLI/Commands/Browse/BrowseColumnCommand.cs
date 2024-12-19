e2763709d64ecc6ae95e4f93c39355c6adf32098
Alberto Spelta <albertospelta@users.noreply.github.com>
2024-12-19 15:24:58 +0100

    Update command descriptions to use singular form
    Updated the description text in `BrowseColumnCommand.cs` and `BrowseRelationshipCommand.cs` to use the singular form of "column" and "relationship" for improved clarity and accuracy.

--- src/Dax.Vpax.CLI/Commands/Browse/BrowseColumnCommand.cs
+++ src/Dax.Vpax.CLI/Commands/Browse/BrowseColumnCommand.cs
@@ -7,7 +7,7 @@ internal sealed class BrowseColumnCommand : Command
     public static BrowseColumnCommand Instance { get; } = new BrowseColumnCommand();
 
     private BrowseColumnCommand()
-        : base(name: "column", description: "Display columns information")
+        : base(name: "column", description: "Display column information")
     {
         AddAlias("c");
         Handler = new BrowseColumnCommandHandler();
--- src/Dax.Vpax.CLI/Commands/Browse/BrowseRelationshipCommand.cs
+++ src/Dax.Vpax.CLI/Commands/Browse/BrowseRelationshipCommand.cs
@@ -7,7 +7,7 @@ internal sealed class BrowseRelationshipCommand : Command
     public static BrowseRelationshipCommand Instance { get; } = new BrowseRelationshipCommand();
 
     private BrowseRelationshipCommand()
-        : base(name: "relationship", description: "Display relationships information")
+        : base(name: "relationship", description: "Display relationship information")
     {
         AddAlias("r");
         Handler = new BrowseRelationshipCommandHandler();
