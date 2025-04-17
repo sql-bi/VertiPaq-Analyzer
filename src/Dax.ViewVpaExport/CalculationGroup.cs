15d04f6cb70409cf66a58d1bb3be344febfec37d
Alberto Spelta <albertospelta@users.noreply.github.com>
2025-04-17 11:08:35 +0200

    Add class ViewVpa.Model.CalculationGroup

---
+++ src/Dax.ViewVpaExport/CalculationGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace Dax.ViewVpaExport
+{
+    public class CalculationGroup
+    {
+        private readonly Dax.Metadata.CalculationGroup _calculationGroup;
+
+        internal CalculationGroup(Dax.Metadata.CalculationGroup calculationGroup)
+        {
+            _calculationGroup = calculationGroup;
+        }
+
+        public string MultipleOrEmptySelectionExpression => _calculationGroup.MultipleOrEmptySelectionExpression?.Expression;
+        public string MultipleOrEmptySelectionFormatStringExpression => _calculationGroup.MultipleOrEmptySelectionFormatStringExpression?.Expression;
+        public string NoSelectionExpression => _calculationGroup.NoSelectionExpression?.Expression;
+        public string NoSelectionFormatStringExpression => _calculationGroup.NoSelectionFormatStringExpression?.Expression;
+        public int Precedence => _calculationGroup.Precedence;
+        public string TableName => _calculationGroup.Table.TableName.Name;
+    }
+}
