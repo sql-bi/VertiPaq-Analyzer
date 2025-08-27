c74b0e3ac0b29c3f516388bf7518dffbb922c12a
Alberto Spelta <albertospelta@users.noreply.github.com>
2025-08-27 16:45:11 +0200

    Add Metadata.Calendar

---
+++ src/Dax.Metadata/Calendar.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace Dax.Metadata
+{
+    [DebuggerDisplay("{CalendarName.Name,nq}")]
+    public class Calendar
+    {
+        public List<CalendarColumnGroup> CalendarColumnGroups { get; set; } = [];
+
+        public DaxName CalendarName { get; set; }
+
+        public DaxNote Description { get; set; }
+
+        [JsonIgnore]
+        public Table Table { get; set; }
+
+        [OnDeserialized]
+        private void OnDeserializedMethod(StreamingContext context)
+        {
+            foreach (var ccg in CalendarColumnGroups)
+                ccg.Calendar = this;
+        }
+    }
+}
---
+++ src/Dax.Metadata/CalendarColumnGroup.cs
@@ -0,0 +1,27 @@
+﻿using Dax.Metadata.JsonConverters;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Dax.Metadata
+{
+    [JsonConverter(typeof(CalendarColumnGroupCustomCreationConverter))]
+    public abstract class CalendarColumnGroup
+    {
+        [JsonIgnore]
+        public Calendar Calendar { get; set; }
+    }
+
+    public class TimeRelatedColumnGroup : CalendarColumnGroup
+    {
+        public List<Column> Columns { get; set; } = [];
+    }
+
+    public class TimeUnitColumnAssociation : CalendarColumnGroup
+    {
+        public List<Column> AssociatedColumns { get; set; } = [];
+
+        public Column PrimaryColumn { get; set; }
+
+        public TimeUnit TimeUnit { get; set; }
+    }
+}
