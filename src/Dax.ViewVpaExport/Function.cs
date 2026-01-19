b62e0002bbd2362eebc03180d66c01d635cab6be
Alberto Spelta <albertospelta@users.noreply.github.com>
2026-01-19 09:18:45 +0100

    Add [JsonIgnore]

--- src/Dax.ViewVpaExport/Function.cs
+++ src/Dax.ViewVpaExport/Function.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Dax.ViewVpaExport
 {
     public class Function
     {
+        [JsonIgnore]
         private readonly Dax.Metadata.Function _function;
+
         internal Function(Dax.Metadata.Function function)
         {
             _function = function;
