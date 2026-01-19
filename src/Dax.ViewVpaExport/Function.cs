8f2fac661a88b17f58157088d003f9b966496ab3
Alberto Spelta <albertospelta@users.noreply.github.com>
2026-01-19 10:52:38 +0100

    Add FunctionExpression property

--- src/Dax.ViewVpaExport/Function.cs
+++ src/Dax.ViewVpaExport/Function.cs
@@ -1,10 +1,4 @@
-﻿using Dax.Metadata;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Dax.ViewVpaExport
 {
@@ -18,9 +12,9 @@ namespace Dax.ViewVpaExport
             _function = function;
         }
 
-        public string FunctionName => _function.FunctionName.Name;
         public string Description => _function.Description?.Note;
-        public bool IsHidden => _function.IsHidden;
-        
+        public string FunctionExpression => _function.FunctionExpression?.Expression;
+        public string FunctionName => _function.FunctionName.Name;
+        public bool IsHidden => _function.IsHidden;        
     }
 }
