5e9306130f445a3f36713a0297522de04a44cfc9
Marco Russo <marco@sqlbi.com>
2026-01-19 10:55:55 +0100

    Added functions to ViewVpaExport (#219)

---
+++ src/Dax.ViewVpaExport/Function.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Dax.ViewVpaExport
+{
+    public class Function
+    {
+        [JsonIgnore]
+        private readonly Dax.Metadata.Function _function;
+
+        internal Function(Dax.Metadata.Function function)
+        {
+            _function = function;
+        }
+
+        public string Description => _function.Description?.Note;
+        public string FunctionExpression => _function.FunctionExpression?.Expression;
+        public string FunctionName => _function.FunctionName.Name;
+        public bool IsHidden => _function.IsHidden;        
+    }
+}
